6038a48e0c106e72c3595b3474c03c3289e8aed9
marvin-datalysis <marvin.cubias@datalysisgroup.com>
2024-07-27 23:04:55 -0600

    dockerfile

--- your_first_project/data_loaders/load_sales_csv.py
+++ your_first_project/data_loaders/load_sales_csv.py
@@ -11,9 +11,28 @@ if 'test' not in globals():
 @data_loader
 def load_data_from_file(*args, **kwargs):
     filepath = 'your_first_project/Ventas Octubre-Junio Input.csv'
+    
+    df_limpieza = data
 
-    return FileIO().load(filepath,encoding="cp1252")
+    new_names = [transformCol(col) for col in df_limpieza.columns]
+    df_limpieza.columns = new_names
+    df_limpieza = df_limpieza.applymap(transform_data)
 
+    df_limpieza.to_csv('Limpieza_Ventas.csv', index=False)
+
+    return df_limpieza
+
+def transformCol(col_name):
+    col_name = col_name.upper()
+    col_name = unidecode(col_name)
+    col_name = col_name.replace(' ', '_')
+    col_name = re.sub(r'\W+', '', col_name)
+    return col_name
+
+def transform_data(value):
+    if isinstance(value, str):
+        return value.capitalize()
+    return value
 
 @test
 def test_output(output, *args) -> None:
